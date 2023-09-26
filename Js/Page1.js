4d3e481201543b714a6ffa390f3e38f102cdd361
Cody Bonner <bonner_cody94@yahoo.com>
2023-09-26 01:29:55 +0000

    updated java-page1

--- Js/Page1.js
+++ Js/Page1.js
@@ -1,20 +1,32 @@
 "use strict";
-const form = document.getElementById("userName-container");
 
-function UserInformation(name,productionValues,productionDates) {
+const globalVariables = {
+  form: document.getElementById("userName-container"),
+};
+
+function UserInformation(name, productionValue, productionDate) {
   //add conditionals for populating two arrays
   this.name = name;
-  this.productionValueArray = [];
-  this.productionDatesArray = [];
+  this.productionValue = productionValue;
+  this.productionDate = productionDate;
 }
+UserInformation.prototype.render = function () {
+  const displayedName = document.createElement("p");
+  const displayedValue = document.createElement("p");
+  const displayedDated = document.createElement("p");
 
+  displayedName.textContent = `Username: ${this.name}`;
+  displayedValue.textContent = `Todays production value: ${this.productionValue}`;
+  displayedDated.textContent = `Date: ${this.productionDate}`;
+  globalVariables.form.appendChild(displayedName);
+  displayedName.appendChild(displayedValue);
+  displayedValue.appendChild(displayedDated);
+};
 
 //function event designed to locally store userName, production and date when submit is clicked
-function  infoLog(event){
+function infoLog(event) {
   event.preventDefault();
-  
-
-
+  localStorage.clear();
 
   let userInput = document.getElementById("name");
   let userName = userInput.value;
@@ -26,31 +38,37 @@ function  infoLog(event){
 
   let dateInput = document.getElementById("dateValue");
   let dateValue = dateInput.value;
-  localStorage.setItem("Date:", dateValue);
+  localStorage.setItem("Date", dateValue);
 
-  
+  const inputName = document.createElement("p");
+  const inputMilkValue = document.createElement("p");
+  const inputDate = document.createElement("p");
+  inputName.textContent = `Username: ${userName}`;
+  inputMilkValue.textContent = `Todays production value: ${productionValue}`;
+  inputDate.textContent = `Date: ${dateValue}`;
+  globalVariables.form.appendChild(inputName);
+  inputName.appendChild(inputMilkValue);
+  inputMilkValue.appendChild(inputDate);
 
+  event.target.reset();
+}
+//function to render specific information from userInformation
 
-  event.target.reset();  
-  
+function persistData() {
+  let retrievedName = localStorage.getItem("user");
+  let retrievedMilkValue = localStorage.getItem("production-value");
+  let retrievedDate = localStorage.getItem("Date");
+  const user1 = new UserInformation(
+    retrievedName,
+    retrievedMilkValue,
+    retrievedDate
+  );
+  user1.render();
 }
 
-UserInformation.prototype.render = function(){
-  const displayedName = document.createElement("p");
-  displayedName.textContent = this.name;
-  form.appendChild(displayedName);
-  
+//retrieves and stores information from local storage and places it in instance of
+//userInformation constructor that's been named user1
 
-  
-};
+globalVariables.form.addEventListener("submit", infoLog);
 
-let retrievedName = localStorage.getItem("user");
-console.log(retrievedName);
-const user1 = new UserInformation(retrievedName);
-user1.render();
-
-
-
-
-form.addEventListener("submit",infoLog);
-
+persistData();
