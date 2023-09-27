e57987fe65ddcd69e2ecf4b74483b2f0effe6aa4
Cody Bonner <bonner_cody94@yahoo.com>
2023-09-27 00:08:03 +0000

    updated displayed information based on user input

--- Js/Page1.js
+++ Js/Page1.js
@@ -4,24 +4,25 @@ const globalVariables = {
   form: document.getElementById("userName-container"),
 };
 
-
 //function event designed to locally store userName, production and date when submit is clicked
 function infoLog(event) {
   event.preventDefault();
   localStorage.clear();
-  
+
   let userInput = document.getElementById("name");
   let userName = `Username:${userInput.value}`;
   localStorage.setItem("user", userName);
-  
+
   let productionInput = document.getElementById("productionValue");
   let productionValue = `Today's Production Value: ${productionInput.value}`;
   localStorage.setItem("production-value", productionValue);
-  
+
   let dateInput = document.getElementById("dateValue");
   let dateValue = `Today's Date: ${dateInput.value}`;
   localStorage.setItem("Date", dateValue);
+
   
+
   const inputName = document.createElement("p");
   const inputMilkValue = document.createElement("p");
   const inputDate = document.createElement("p");
@@ -31,10 +32,24 @@ function infoLog(event) {
   globalVariables.form.appendChild(inputName);
   inputName.appendChild(inputMilkValue);
   inputMilkValue.appendChild(inputDate);
-  
+
+  if (productionInput.value < 18) {
+    const lowLevel = document.createElement("p");
+    lowLevel.textContent = `Your milk production is ${productionInput.value} and is considered low, please click the low production image below.`;
+    inputMilkValue.appendChild(lowLevel);
+  }else if(productionInput.value > 19 && productionInput.value < 25)
+  {
+    const mediumLevel = document.createElement("p");
+    mediumLevel.textContent = `Your milk production is ${productionInput.value} and is considered medium, please click the medium production image below`;
+    inputMilkValue.appendChild(mediumLevel);
+  }else if(productionInput.value > 25){
+    const highLevel = document.createElement("p");
+    highLevel.textContent = `Your milk production is ${productionInput.value} and is considered high, please click the high production image below`;
+    inputMilkValue.appendChild(highLevel);
+  }
+
   globalVariables.form.removeChild(globalVariables.form.children[1]);
   event.target.reset();
-  
 }
 
 function UserInformation(name, productionValue, productionDate) {
@@ -47,14 +62,15 @@ UserInformation.prototype.render = function () {
   const displayedName = document.createElement("p");
   const displayedValue = document.createElement("p");
   const displayedDated = document.createElement("p");
-  
-  
+
   displayedName.textContent = `${this.name}`;
   displayedValue.textContent = `${this.productionValue}`;
   displayedDated.textContent = `${this.productionDate}`;
   globalVariables.form.appendChild(displayedName);
   displayedName.appendChild(displayedValue);
   displayedValue.appendChild(displayedDated);
+
+  
 };
 globalVariables.form.addEventListener("submit", infoLog);
 //function to render specific information from userInformation
